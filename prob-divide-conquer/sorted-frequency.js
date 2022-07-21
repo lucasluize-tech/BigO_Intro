525c747c1394ab64b3cb9f570cbbce3cb6814ffc
Lucas Luize <lucasluize.tech@gmail.com>
2022-07-21 10:14:25 -0400

    sorted-frequency done

--- prob-divide-conquer/sorted-frequency.js
+++ prob-divide-conquer/sorted-frequency.js
@@ -1,5 +1,36 @@
-function sortedFrequency() {
+function sortedFrequency(arr, val) {
+  let count = 0;
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    //find the mid index
+    let mid = Math.floor((start + end) / 2);
 
+    // if mid equals to value
+    if (arr[mid] === val) {
+      // pointers
+      let left = mid - 1;
+      let right = mid + 1;
+      count++;
+      while (arr[left] === val) {
+        count++;
+        left -= 1;
+      }
+      while (arr[right] === val) {
+        count++;
+        right += 1;
+      }
+      return count;
+    }
+
+    // if mid less than value
+    if (arr[mid] < val) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return -1;
 }
 
-module.exports = sortedFrequency
+module.exports = sortedFrequency;
