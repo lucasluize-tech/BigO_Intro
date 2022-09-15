fd9987118b258f43da67548f1164393fd41894e4
Lucas Luize <lucasluize.tech@gmail.com>
2022-09-15 11:19:15 -0400

    two-array-objects done

--- prob-freq-count-pointers/two-array-object.js
+++ prob-freq-count-pointers/two-array-object.js
@@ -1,3 +1,12 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, vals) {
+  // should return an object create from the keys and values
+  let obj = {};
+  for (let i = 0; i < keys.length; i++) {
+    // if not enough values, the rest must be null val
+    obj[keys[i]] = vals[i] || null;
+  }
+  return obj;
 }
+
+module.exports = twoArrayObject;
