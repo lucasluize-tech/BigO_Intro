6808b5cb3bdd4047f0248709014e97daa2534151
Lucas Luize <lucasluize.tech@gmail.com>
2022-09-15 11:42:44 -0400

    sameFrequency done

--- prob-freq-count-pointers/same-frequency.js
+++ prob-freq-count-pointers/same-frequency.js
@@ -1,3 +1,8 @@
-// add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(int1, int2) {
+  const n1 = int1.toString().split("").sort();
+  const n2 = int2.toString().split("").sort();
+  if (n1.length !== n2.length) return false;
+  if (n1[0] !== n2[0] || n1[n1.length - 1] !== n2[n2.length - 1]) return false;
+  return true;
 }
+module.exports = sameFrequency;
