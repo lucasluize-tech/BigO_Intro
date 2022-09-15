5d677fb7bd2b71b27350ab1e4a90bf58d8176c38
Lucas Luize <lucasluize.tech@gmail.com>
2022-09-15 14:49:49 -0400

    pivot-index done

--- prob-freq-count-pointers/pivot-index.js
+++ prob-freq-count-pointers/pivot-index.js
@@ -1,3 +1,15 @@
-// add whatever parameters you deem necessary
-function pivotIndex() {
+function pivotIndex(arr) {
+  const sum = arr.reduce((prev, curr) => prev + curr, 0);
+  let leftSum = 0;
+  let idx = 0;
+
+  while (idx < arr.length) {
+    if (sum - leftSum - arr[idx] === leftSum) return idx;
+    leftSum += arr[idx];
+    idx++;
+    console.log(leftSum);
+  }
+  return -1;
 }
+
+module.exports = pivotIndex;
