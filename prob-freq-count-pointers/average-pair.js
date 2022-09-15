08c0f0bd1d2fcc309f535854acd5283d2e766360
Lucas Luize <lucasluize.tech@gmail.com>
2022-09-15 11:11:56 -0400

    average-pair done

--- prob-freq-count-pointers/average-pair.js
+++ prob-freq-count-pointers/average-pair.js
@@ -1,11 +1,17 @@
 function averagePair(arr, avg) {
   // check for a pair in the Array that has the avg.
   let start = 0;
+  let end = arr.length - 1;
   if (arr.length === 0) return false;
   // create pointers to check
-  while (arr.length > start) {
-    if (arr.includes(avg * 2 - start)) return true;
-    start++;
+  while (start < end) {
+    let target = avg * 2;
+    if (arr[start] + arr[end] === target) return true;
+    if (arr[start] + arr[end] > target) {
+      end--;
+    } else if (arr[start] + arr[end] < target) {
+      start++;
+    }
   }
   return false;
 }
