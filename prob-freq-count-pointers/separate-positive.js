22eb3ba5c3ad598fae36cee4f4b3c74ecd83b9e5
Lucas Luize <lucasluize.tech@gmail.com>
2022-09-15 12:11:23 -0400

    separatePositive done

--- prob-freq-count-pointers/separate-positive.js
+++ prob-freq-count-pointers/separate-positive.js
@@ -1,3 +1,23 @@
-// add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(arr) {
+  arr = arr.sort((a, b) => a - b);
+  // lets create some pointers;
+  let start = 0;
+  let count = arr.length;
+  console.log(arr);
+  while (count !== 0) {
+    // let's work with the negatives first
+    if (arr[start] < 0) {
+      arr.push(arr[start]);
+      arr.shift();
+      count--;
+    }
+
+    if (arr[start] > 0) {
+      count--;
+    }
+  }
+
+  return arr;
 }
+
+module.exports = separatePositive;
