babe539e935036bcfb1d6da3cb58c956f5854abd
Lucas Luize <lucasluize.tech@gmail.com>
2022-09-15 14:23:57 -0400

    longest-fall done

--- prob-freq-count-pointers/longest-fall.js
+++ prob-freq-count-pointers/longest-fall.js
@@ -1,3 +1,23 @@
-// add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(arr) {
+  let longest = 0;
+  let count = 1;
+  if (arr.length === 1) return 1;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] === arr[i + 1]) {
+      longest = count > longest ? (longest = count) : longest;
+      count = 1;
+    }
+
+    if (arr[i + 1] > arr[i]) {
+      longest = count > longest ? (longest = count) : longest;
+      count = 1;
+    }
+    if (arr[i] > arr[i + 1]) {
+      count++;
+    }
+    longest = count > longest ? (longest = count) : longest;
+  }
+  return longest;
 }
+
+module.exports = longestFall;
