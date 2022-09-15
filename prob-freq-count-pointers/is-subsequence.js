c301c46df9dded47f074bf241308e6b985eac2ed
Lucas Luize <lucasluize.tech@gmail.com>
2022-09-15 12:36:31 -0400

    isSubsequence done

--- prob-freq-count-pointers/is-subsequence.js
+++ prob-freq-count-pointers/is-subsequence.js
@@ -1,3 +1,28 @@
-// add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(word, msg) {
+  // make arrays
+  word = word.split("");
+  msg = msg.split("");
+  let wordObj = {};
+  let msgObj = {};
+  let count = 1;
+  // make an object that keeps an order priority ['h','e','l','l','o'] => {h:1,e:2,l:3,o:4}
+  msg.forEach((l) => {
+    msgObj[l] = count;
+    count++;
+  });
+  count = 1;
+
+  word.forEach((l) => {
+    wordObj[l] = count;
+    count++;
+  });
+  // compare priorities and if letter is included in object
+
+  for (let letter in wordObj) {
+    if (msgObj[letter] < wordObj[letter]) return false;
+    if (msgObj[letter] === undefined || null) return false;
+  }
+  return true;
 }
+
+module.exports = isSubsequence;
