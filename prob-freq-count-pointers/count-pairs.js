67667a81ea646da339745aad0a0de9c1fa7c760a
Lucas Luize <lucasluize.tech@gmail.com>
2022-09-15 13:00:16 -0400

    count-pairs done

--- prob-freq-count-pointers/count-pairs.js
+++ prob-freq-count-pointers/count-pairs.js
@@ -1,3 +1,32 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+// o(n)
+
+function countPairs(arr, sum) {
+  // sort first
+  arr = arr.sort((a, b) => a - b);
+  let count = 0; // keep count
+  // make pointers
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start < end) {
+    // if sum
+    if (arr[start] + arr[end] === sum) {
+      count++;
+      start++;
+      end--;
+      continue;
+    }
+    // if more than sum
+    if (arr[start] + arr[end] > sum) {
+      end--;
+    }
+    // if less than sum
+    if (arr[start] + arr[end] < sum) {
+      start++;
+    }
+  }
+
+  return count;
 }
+
+module.exports = countPairs;
